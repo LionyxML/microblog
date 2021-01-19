8930bb886b163f5c7ab30bdce345426ecaf2dcdf
LionyxML <rahul.juliato@gmail.com>
2021-01-19 19:10:30 -0300

    Added moment.js - made usertime friendly

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,8 @@ from logging.handlers import RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 
 
 app = Flask(__name__)
@@ -26,6 +28,8 @@ login.login_view = 'login'
 mail = Mail(app)
 
 bootstrap = Bootstrap(app)
+
+moment = Moment(app)
 
 from app import routes, models, errors
 
